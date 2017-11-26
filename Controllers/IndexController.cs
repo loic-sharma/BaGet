6928b022e6de7b626b3bf007f7290adc47f77ff1
Loïc Sharma <sharma.loic@gmail.com>
2017-11-26 09:36:43 -0800

    Initial commit

---
+++ Controllers/IndexController.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Runtime.Serialization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaGet.Controllers
+{
+    /// <summary>
+    /// The NuGet Service Index. This aids NuGet client to discover this server's services.
+    /// </summary>
+    public class IndexController
+    {
+        // GET v3/index
+        [HttpGet]
+        public object Get()
+        {
+            // Documentation: https://docs.microsoft.com/en-us/nuget/api/overview
+            // NuGet.org: https://api.nuget.org/v3-index/index.json
+            return new
+            {
+                Version = "3.0.0-beta.1",
+                Resources = new[]
+                {
+                    // Required
+                    new ServiceResource("PackagePublish/2.0.0", new Uri("https://google.com")),
+                    new ServiceResource("SearchQueryService/3.0.0-rc", new Uri("https://google.com")),
+                    new ServiceResource("RegistrationsBaseUrl/3.0.0-rc", new Uri("https://google.com")),
+                    new ServiceResource("PackageBaseAddress/3.0.0", new Uri("https://google.com")),
+
+                    // Optional
+                    //new ServiceResource("SearchAutocompleteService/3.0.0-rc", new Uri("https://google.com")),
+                    //new ServiceResource("ReportAbuseUriTemplate/3.0.0-rc", new Uri("https://google.com")),
+                    //new ServiceResource("Catalog/3.0.0", new Uri("https://google.com"))
+                }
+            };
+        }
+
+        private class ServiceResource
+        {
+            public ServiceResource(string type, Uri id, string comment = null)
+            {
+                Id = id ?? throw new ArgumentNullException(nameof(id));
+                Type = type ?? throw new ArgumentNullException(nameof(type));
+                Comment = comment ?? string.Empty;
+            }
+
+            [DataMember(Name = "@id")]
+            public Uri Id { get; }
+
+            [DataMember(Name = "@type")]
+            public string Type { get; }
+
+            public string Comment { get; }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+# BaGet
+
+## About
+
+A fresh reimplementation of the NuGet service.
+
+## Installation
+
+TODO
+
+## License
+
+TODO
