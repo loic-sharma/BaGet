bc7d8f4d987d0595e53147b5aa5a58fd72d5b3ad
Werner Mairl <werner.mairl@gmail.com>
2018-07-05 07:38:48 +0200

    First test for Controller-Classes, using a (in memory) TestServer. Log-Content created by the SUT (software under test) is send to Xunit.ITestOutputHelper

---
+++ src/tests/BaGet.Tests/PackageControllerTest.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace BaGet.Tests
+{
+
+    public class PackageControllerTest 
+    {
+        protected readonly ITestOutputHelper Helper;
+
+        readonly string IndexUrlFormatString = "v3/package/{0}/index.json";
+
+        public PackageControllerTest(ITestOutputHelper helper)
+        {
+            Helper = helper ?? throw new ArgumentNullException(nameof(helper));
+        }
+
+        [Theory]
+        [InlineData("id01")]
+        [InlineData("id02")]
+        public async Task AskEmptyServerForNotExistingPackageID(string packageID)
+        {
+            using (TestServer server = TestServerBuilder.Empty().TraceToTestOutputHelper(this.Helper,LogLevel.Error).Build())
+            {
+                //Ask Empty Storage for a not existings ID
+                var response = await  server.CreateClient().GetAsync(string.Format(IndexUrlFormatString, packageID));
+                Assert.Equal(System.Net.HttpStatusCode.NotFound, response.StatusCode);
+            }
+        }
+    }
+}
