547d2af3c41f11e9d127633a8f663877c77678ab
Werner Mairl <werner.mairl@gmail.com>
2018-07-12 06:19:56 +0200

    add test project for BaGet.Services.Mirror - tests for IConfiguration validation

---
+++ src/tests/BaGet.Services.Mirror.Tests/ServiceCollectionExtensionsTest.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+using BaGet.Core.Configuration;
+using BaGet.Extensions;
+using BaGet.Services.Mirror.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BaGet.Services.Mirror.Tests
+{
+    public class ServiceCollectionExtensionsTest
+    {
+
+        [Fact]
+        public void AskServiceProviderForNotConfiguredMirrorOptions()
+        {
+            ServiceProvider provider = new ServiceCollection()
+                .AddBaGetContext() //Required
+                .AddMirrorServices() //Method Under Test!
+                .BuildServiceProvider();
+
+            InvalidOperationException expected = Assert.Throws<InvalidOperationException>(
+                () => provider.GetRequiredService<IMirrorService>()
+            );
+            Assert.Contains(nameof(BaGetOptions.Mirror), expected.Message);
+        }
+
+        [Fact]
+        public void AskServiceProviderForWellConfiguredMirrorOptions()
+        {
+            //Create a IConfiguration with a minimal "Mirror" object.
+            KeyValuePair<string, string> initialData = new KeyValuePair<string, string>("Mirror:EnableReadThroughCaching", false.ToString());
+            IConfiguration configuration = new ConfigurationBuilder().AddInMemoryCollection(new KeyValuePair<string, string>[] { initialData }).Build();
+
+            ServiceProvider provider = new ServiceCollection()
+                .Configure<BaGetOptions>(configuration)
+                .AddMirrorServices() //Method Under Test!
+                .BuildServiceProvider();
+
+            Assert.NotNull(provider.GetRequiredService<IMirrorService>());
+        }
+
+    }
+}
---
+++ src/tests/BaGet.Tests/ServiceCollectionExtensionsTest.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using BaGet.Core.Configuration;
+using BaGet.Core.Entities;
+using BaGet.Entities;
+using BaGet.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace BaGet.Tests
+{
+    public class ServiceCollectionExtensionsTest
+    {
+
+        [Fact]
+
+        public void AskServiceProviderForNotConfiguredDatabaseOptions()
+        {
+            ServiceProvider provider = new ServiceCollection()
+                .AddBaGetContext() //Method Under Test
+                .BuildServiceProvider();
+
+            InvalidOperationException expected = Assert.Throws<InvalidOperationException>( 
+                () => provider.GetRequiredService<IContext>().Database
+            );
+
+            Assert.Contains(nameof(BaGetOptions.Database), expected.Message);        
+        }
+
+
+        [Fact]
+        public void AskServiceProviderForWellConfiguredDatabaseOptions()
+        {
+            //Create a IConfiguration with a minimal "Database" object.
+            Dictionary<string, string> initialData = new Dictionary<string, string>();
+            initialData.Add("Database:Type", DatabaseType.Sqlite.ToString());
+            initialData.Add("Database:ConnectionString", "blabla");
+
+            IConfiguration configuration = new ConfigurationBuilder().AddInMemoryCollection(initialData).Build();
+
+            ServiceProvider provider = new ServiceCollection()
+                .Configure<BaGetOptions>(configuration)
+                .AddBaGetContext() //Method Under Test
+                .BuildServiceProvider();
+
+            Assert.NotNull(provider.GetRequiredService<IContext>());
+        }
+
+
+
+
+        [Fact]
+        public void AskServiceProviderForWellConfiguredSqliteContext()
+        {
+            //Create a IConfiguration with a minimal "Database" object.
+            Dictionary<string, string> initialData = new Dictionary<string, string>();
+            initialData.Add("Database:Type", DatabaseType.Sqlite.ToString());
+            initialData.Add("Database:ConnectionString", "blabla");
+
+            IConfiguration configuration = new ConfigurationBuilder().AddInMemoryCollection(initialData).Build();
+
+            ServiceProvider provider = new ServiceCollection()
+                .Configure<BaGetOptions>(configuration)
+                .AddBaGetContext() //Method Under Test
+                .BuildServiceProvider();
+
+            Assert.NotNull(provider.GetRequiredService<SqliteContext>());
+        }
+
+        [Fact]
+        public void AskServiceProviderForSqliteContextWithMissingConnectionString()
+        {
+            //Create a IConfiguration with a minimal "Database" object.
+            Dictionary<string, string> initialData = new Dictionary<string, string>();
+            initialData.Add("Database:Type", DatabaseType.Sqlite.ToString());
+            initialData.Add("Database:ConnectionString", string.Empty); //empty equals missing!
+
+            IConfiguration configuration = new ConfigurationBuilder().AddInMemoryCollection(initialData).Build();
+
+            ServiceProvider provider = new ServiceCollection()
+                .Configure<BaGetOptions>(configuration)
+                .AddBaGetContext() //Method Under Test
+                .BuildServiceProvider();
+
+            InvalidOperationException expected = Assert.Throws<InvalidOperationException>(
+                           () => provider.GetRequiredService<SqliteContext>().Database
+                       );
+
+            Assert.Contains(nameof(DatabaseOptions.ConnectionString), expected.Message);
+        }
+
+        [Theory]
+        [InlineData("<invalid>")]
+        [InlineData("")]
+        [InlineData(" ")]
+        public void AskServiceProviderForInvalidDatabaseType(string databaseType)
+        {
+            //Create a IConfiguration with a minimal "Database" object.
+            Dictionary<string, string> initialData = new Dictionary<string, string>();
+            initialData.Add("Database:Type", databaseType);
+
+            IConfiguration configuration = new ConfigurationBuilder().AddInMemoryCollection(initialData).Build();
+
+            ServiceProvider provider = new ServiceCollection()
+                .Configure<BaGetOptions>(configuration)
+                .AddBaGetContext() //Method Under Test
+                .BuildServiceProvider();
+
+            InvalidOperationException expected = Assert.Throws<InvalidOperationException>(
+                           () => provider.GetRequiredService<IContext>().Database
+                       );
+            //currently the DatabaseType is implemented as Enum => configuration System uses deserialization and we get a 
+            //"Failed to convert '<invalid>' to type 'BaGet.Core.Configuration.DatabaseType" message in case of invalid Type.
+            //not sure if it is worth to improve that!
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
