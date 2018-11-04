896f145444f12b4513034e9810ae2162ded38b9f
Loic Sharma <sharma.loic@gmail.com>
2018-11-04 12:05:55 -0800

    Add package deletion service tests

--- tests/BaGet.Core.Tests/Services/PackageDeletionServiceTests.cs
+++ tests/BaGet.Core.Tests/Services/PackageDeletionServiceTests.cs
@@ -4,12 +4,16 @@ using BaGet.Core.Services;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
+using NuGet.Versioning;
 using Xunit;
 
 namespace BaGet.Core.Tests.Services
 {
     public class PackageDeletionServiceTests
     {
+        private static readonly string PackageId = "Package";
+        private static readonly NuGetVersion PackageVersion = new NuGetVersion("1.0.0");
+
         private readonly Mock<IPackageService> _packages;
         private readonly Mock<IPackageStorageService> _storage;
 
@@ -37,7 +41,29 @@ namespace BaGet.Core.Tests.Services
         [InlineData(true)]
         public async Task WhenUnlist_ReturnsTrueOnlyIfPackageExists(bool packageExists)
         {
-            await Task.Yield();
+            // Arrange
+            _options.PackageDeletionBehavior = PackageDeletionBehavior.Unlist;
+
+            _packages
+                .Setup(p => p.UnlistPackageAsync(PackageId, PackageVersion))
+                .ReturnsAsync(packageExists);
+
+            // Act
+            var result = await _target.TryDeletePackageAsync(PackageId, PackageVersion);
+
+            // Assert
+            Assert.Equal(packageExists, result);
+
+            _packages.Verify(
+                p => p.UnlistPackageAsync(PackageId, PackageVersion),
+                Times.Once);
+
+            _packages.Verify(
+                p => p.HardDeletePackageAsync(It.IsAny<string>(), It.IsAny<NuGetVersion>()),
+                Times.Never);
+            _storage.Verify(
+                s => s.DeleteAsync(It.IsAny<string>(), It.IsAny<NuGetVersion>()),
+                Times.Never);
         }
 
         [Theory]
@@ -46,8 +72,43 @@ namespace BaGet.Core.Tests.Services
 
         public async Task WhenHardDelete_ReturnsTrueOnlyIfPackageExists(bool packageExists)
         {
-            // TODO: Test that hard delete ALWAYS removes from storage, regardless of packageExists
-            await Task.Yield();
+            // Arrange
+            _options.PackageDeletionBehavior = PackageDeletionBehavior.HardDelete;
+
+            var step = 0;
+            var databaseStep = -1;
+            var storageStep = -1;
+
+            _packages
+                .Setup(p => p.HardDeletePackageAsync(PackageId, PackageVersion))
+                .Callback(() => databaseStep = step++)
+                .ReturnsAsync(packageExists);
+
+            _storage
+                .Setup(s => s.DeleteAsync(PackageId, PackageVersion))
+                .Callback(() => storageStep = step++)
+                .Returns(Task.CompletedTask);
+
+            // Act
+            var result = await _target.TryDeletePackageAsync(PackageId, PackageVersion);
+
+            // Assert - The database step MUST happen before the storage step.
+            Assert.Equal(packageExists, result);
+            Assert.Equal(0, databaseStep);
+            Assert.Equal(1, storageStep);
+
+            // The storage deletion should happen even if the package couldn't
+            // be found in the database. This ensures consistency.
+            _packages.Verify(
+                p => p.HardDeletePackageAsync(PackageId, PackageVersion),
+                Times.Once);
+            _storage.Verify(
+                s => s.DeleteAsync(PackageId, PackageVersion),
+                Times.Once);
+
+            _packages.Verify(
+                p => p.UnlistPackageAsync(It.IsAny<string>(), It.IsAny<NuGetVersion>()),
+                Times.Never);
         }
     }
 }
