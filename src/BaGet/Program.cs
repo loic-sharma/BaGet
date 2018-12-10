bfd3a9813c3b05ce7e0b1f8e56e1ad3cf9d51356
Alexander Fast <mizipzor@gmail.com>
2018-12-10 11:33:21 +0100

    Added env var to specify other appsettings

--- src/BaGet/Program.cs
+++ src/BaGet/Program.cs
@@ -1,8 +1,10 @@
-﻿using BaGet.Core.Mirror;
+using System;
+using BaGet.Core.Mirror;
 using BaGet.Extensions;
 using McMaster.Extensions.CommandLineUtils;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -51,6 +53,17 @@ namespace BaGet
                     // Remove the upload limit from Kestrel. If needed, an upload limit can
                     // be enforced by a reverse proxy server, like IIS.
                     options.Limits.MaxRequestBodySize = null;
+                })
+                .ConfigureAppConfiguration((builderContext, config) =>
+                {
+                    var root = Environment.GetEnvironmentVariable("APPSETTINGS_ROOT");
+                    if (!string.IsNullOrEmpty(root))
+                    {
+                        var env = builderContext.HostingEnvironment;
+                        config.Sources.Clear();
+                        config.AddJsonFile($"{root}/appsettings.json", optional: false, reloadOnChange: true)
+                            .AddJsonFile($"{root}/appsettings.{env.EnvironmentName}.json", optional: true, reloadOnChange: true);
+                    }
                 });
 
         public static IHostBuilder CreateHostBuilder(string[] args)
