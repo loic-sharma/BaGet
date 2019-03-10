bd57d977c512b7b528b0bd048de68f1c8b42181c
Loic Sharma <sharma.loic@gmail.com>
2019-03-10 00:12:49 -0800

    Fix namespace

--- src/BaGet.Database.Sqlite/Migrations/20180804082808_Initial.cs
+++ src/BaGet.Database.Sqlite/Migrations/20180804082808_Initial.cs
@@ -1,7 +1,7 @@
 using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace BaGet.Database.Sqlite
+namespace BaGet.Database.Sqlite.Migrations
 {
     public partial class Initial : Migration
     {
