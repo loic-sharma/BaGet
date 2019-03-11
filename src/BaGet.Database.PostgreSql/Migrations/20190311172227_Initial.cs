bafbc5b4be6336c711c5efb44cddbd7a9554bb7d
Roma Marusyk <romamarusyk@gmail.com>
2019-03-11 19:24:27 +0200

    Code review refactoring

---
+++ src/BaGet.Database.PostgreSql/Migrations/20190311172227_Initial.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace BaGet.Database.PostgreSql.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:citext", ",,");
+
+            migrationBuilder.CreateTable(
+                name: "Packages",
+                columns: table => new
+                {
+                    Key = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Id = table.Column<string>(type: "citext", maxLength: 128, nullable: false),
+                    Authors = table.Column<string>(maxLength: 4000, nullable: true),
+                    Description = table.Column<string>(maxLength: 4000, nullable: true),
+                    Downloads = table.Column<long>(nullable: false),
+                    HasReadme = table.Column<bool>(nullable: false),
+                    IsPrerelease = table.Column<bool>(nullable: false),
+                    Language = table.Column<string>(maxLength: 20, nullable: true),
+                    Listed = table.Column<bool>(nullable: false),
+                    MinClientVersion = table.Column<string>(maxLength: 44, nullable: true),
+                    Published = table.Column<DateTime>(nullable: false),
+                    RequireLicenseAcceptance = table.Column<bool>(nullable: false),
+                    SemVerLevel = table.Column<int>(nullable: false),
+                    Summary = table.Column<string>(maxLength: 4000, nullable: true),
+                    Title = table.Column<string>(maxLength: 256, nullable: true),
+                    IconUrl = table.Column<string>(maxLength: 4000, nullable: true),
+                    LicenseUrl = table.Column<string>(maxLength: 4000, nullable: true),
+                    ProjectUrl = table.Column<string>(maxLength: 4000, nullable: true),
+                    RepositoryUrl = table.Column<string>(maxLength: 4000, nullable: true),
+                    RepositoryType = table.Column<string>(maxLength: 100, nullable: true),
+                    Tags = table.Column<string>(maxLength: 4000, nullable: true),
+                    RowVersion = table.Column<byte[]>(rowVersion: true, nullable: true),
+                    Version = table.Column<string>(maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Packages", x => x.Key);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PackageDependencies",
+                columns: table => new
+                {
+                    Key = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Id = table.Column<string>(type: "citext", maxLength: 128, nullable: true),
+                    VersionRange = table.Column<string>(maxLength: 256, nullable: true),
+                    TargetFramework = table.Column<string>(maxLength: 256, nullable: true),
+                    PackageKey = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PackageDependencies", x => x.Key);
+                    table.ForeignKey(
+                        name: "FK_PackageDependencies_Packages_PackageKey",
+                        column: x => x.PackageKey,
+                        principalTable: "Packages",
+                        principalColumn: "Key",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PackageTypes",
+                columns: table => new
+                {
+                    Key = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(type: "citext", maxLength: 512, nullable: true),
+                    Version = table.Column<string>(maxLength: 64, nullable: true),
+                    PackageKey = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PackageTypes", x => x.Key);
+                    table.ForeignKey(
+                        name: "FK_PackageTypes_Packages_PackageKey",
+                        column: x => x.PackageKey,
+                        principalTable: "Packages",
+                        principalColumn: "Key",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TargetFrameworks",
+                columns: table => new
+                {
+                    Key = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Moniker = table.Column<string>(type: "citext", maxLength: 256, nullable: true),
+                    PackageKey = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TargetFrameworks", x => x.Key);
+                    table.ForeignKey(
+                        name: "FK_TargetFrameworks_Packages_PackageKey",
+                        column: x => x.PackageKey,
+                        principalTable: "Packages",
+                        principalColumn: "Key",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PackageDependencies_Id",
+                table: "PackageDependencies",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PackageDependencies_PackageKey",
+                table: "PackageDependencies",
+                column: "PackageKey");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Packages_Id",
+                table: "Packages",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Packages_Id_Version",
+                table: "Packages",
+                columns: new[] { "Id", "Version" },
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PackageTypes_Name",
+                table: "PackageTypes",
+                column: "Name");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PackageTypes_PackageKey",
+                table: "PackageTypes",
+                column: "PackageKey");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TargetFrameworks_Moniker",
+                table: "TargetFrameworks",
+                column: "Moniker");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TargetFrameworks_PackageKey",
+                table: "TargetFrameworks",
+                column: "PackageKey");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PackageDependencies");
+
+            migrationBuilder.DropTable(
+                name: "PackageTypes");
+
+            migrationBuilder.DropTable(
+                name: "TargetFrameworks");
+
+            migrationBuilder.DropTable(
+                name: "Packages");
+        }
+    }
+}
