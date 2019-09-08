053165cad3139d271f9bd26e633aa996b6024f09
Loic Sharma <sharma.loic@gmail.com>
2019-09-07 20:51:05 -0700

    Update

--- src/BaGet.Protocol/Catalog/NullCursor.cs
+++ src/BaGet.Protocol/Catalog/NullCursor.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 namespace BaGet.Protocol.Catalog
 {
     /// <summary>
-    /// A cursor that does not persist any state.
+    /// A cursor that does not persist any state. Use this with a <see cref="CatalogProcessor"/>
+    /// to process all leafs each time <see cref="CatalogProcessor.ProcessAsync(CancellationToken)"/>
+    /// is called.
     /// </summary>
     public class NullCursor : ICursor
     {
