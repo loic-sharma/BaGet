1532aba35cbdb67d6ebfd3c21af160da2e6ba5ea
Loïc Sharma <sharma.loic@gmail.com>
2019-09-24 07:45:06 -0700

    Move package metadata extraction (#349)

--- src/BaGet.Core/Extensions/PackageArchiveReaderExtensions.cs
+++ src/BaGet.Core/Extensions/PackageArchiveReaderExtensions.cs
@@ -8,6 +8,8 @@ using NuGet.Packaging;
 
 namespace BaGet.Core
 {
+    using NuGetPackageType = NuGet.Packaging.Core.PackageType;
+
     public static class PackageArchiveReaderExtensions
     {
         private static readonly string[] OrderedReadmeFileNames = new[]
@@ -40,6 +42,182 @@ namespace BaGet.Core
             }
 
             throw new InvalidOperationException("Package does not have a readme!");
+        }
+
+        public static Package GetPackageMetadata(this PackageArchiveReader packageReader)
+        {
+            var nuspec = packageReader.NuspecReader;
+
+            (var repositoryUri, var repositoryType) = GetRepositoryMetadata(nuspec);
+
+            return new Package
+            {
+                Id = nuspec.GetId(),
+                Version = nuspec.GetVersion(),
+                Authors = ParseAuthors(nuspec.GetAuthors()),
+                Description = nuspec.GetDescription(),
+                HasReadme = packageReader.HasReadme(),
+                IsPrerelease = nuspec.GetVersion().IsPrerelease,
+                Language = nuspec.GetLanguage() ?? string.Empty,
+                Listed = true,
+                MinClientVersion = nuspec.GetMinClientVersion()?.ToNormalizedString() ?? string.Empty,
+                Published = DateTime.UtcNow,
+                RequireLicenseAcceptance = nuspec.GetRequireLicenseAcceptance(),
+                SemVerLevel = GetSemVerLevel(nuspec),
+                Summary = nuspec.GetSummary(),
+                Title = nuspec.GetTitle(),
+                IconUrl = ParseUri(nuspec.GetIconUrl()),
+                LicenseUrl = ParseUri(nuspec.GetLicenseUrl()),
+                ProjectUrl = ParseUri(nuspec.GetProjectUrl()),
+                RepositoryUrl = repositoryUri,
+                RepositoryType = repositoryType,
+                Dependencies = GetDependencies(nuspec),
+                Tags = ParseTags(nuspec.GetTags()),
+                PackageTypes = GetPackageTypes(nuspec),
+                TargetFrameworks = GetTargetFrameworks(packageReader),
+            };
+        }
+
+        // Based off https://github.com/NuGet/NuGetGallery/blob/master/src/NuGetGallery.Core/SemVerLevelKey.cs
+        private static SemVerLevel GetSemVerLevel(NuspecReader nuspec)
+        {
+            if (nuspec.GetVersion().IsSemVer2)
+            {
+                return SemVerLevel.SemVer2;
+            }
+
+            foreach (var dependencyGroup in nuspec.GetDependencyGroups())
+            {
+                foreach (var dependency in dependencyGroup.Packages)
+                {
+                    if ((dependency.VersionRange.MinVersion != null && dependency.VersionRange.MinVersion.IsSemVer2)
+                        || (dependency.VersionRange.MaxVersion != null && dependency.VersionRange.MaxVersion.IsSemVer2))
+                    {
+                        return SemVerLevel.SemVer2;
+                    }
+                }
+            }
+
+            return SemVerLevel.Unknown;
+        }
+
+        private static Uri ParseUri(string uriString)
+        {
+            if (string.IsNullOrEmpty(uriString)) return null;
+
+            return new Uri(uriString);
+        }
+
+        private static string[] ParseAuthors(string authors)
+        {
+            if (string.IsNullOrEmpty(authors)) return new string[0];
+
+            return authors.Split(new[] { ',', ';', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private static string[] ParseTags(string tags)
+        {
+            if (string.IsNullOrEmpty(tags)) return new string[0];
+
+            return tags.Split(new[] { ',', ';', ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private static (Uri repositoryUrl, string repositoryType) GetRepositoryMetadata(NuspecReader nuspec)
+        {
+            var repository = nuspec.GetRepositoryMetadata();
+
+            if (string.IsNullOrEmpty(repository?.Url) ||
+                !Uri.TryCreate(repository.Url, UriKind.Absolute, out var repositoryUri))
+            {
+                return (null, null);
+            }
+
+            if (repositoryUri.Scheme != Uri.UriSchemeHttps)
+            {
+                return (null, null);
+            }
+
+            if (repository.Type.Length > 100)
+            {
+                throw new InvalidOperationException("Repository type must be less than or equal 100 characters");
+            }
+
+            return (repositoryUri, repository.Type);
+        }
+
+        private static List<PackageDependency> GetDependencies(NuspecReader nuspec)
+        {
+            var dependencies = new List<PackageDependency>();
+
+            foreach (var group in nuspec.GetDependencyGroups())
+            {
+                var targetFramework = group.TargetFramework.GetShortFolderName();
+
+                if (!group.Packages.Any())
+                {
+                    dependencies.Add(new PackageDependency
+                    {
+                        Id = null,
+                        VersionRange = null,
+                        TargetFramework = targetFramework,
+                    });
+                }
+
+                foreach (var dependency in group.Packages)
+                {
+                    dependencies.Add(new PackageDependency
+                    {
+                        Id = dependency.Id,
+                        VersionRange = dependency.VersionRange?.ToString(),
+                        TargetFramework = targetFramework,
+                    });
+                }
+            }
+
+            return dependencies;
+        }
+
+        private static List<PackageType> GetPackageTypes(NuspecReader nuspec)
+        {
+            var packageTypes = nuspec
+                .GetPackageTypes()
+                .Select(t => new PackageType
+                {
+                    Name = t.Name,
+                    Version = t.Version.ToString()
+                })
+                .ToList();
+
+            // Default to the standard "dependency" package type if no types were found.
+            if (packageTypes.Count == 0)
+            {
+                packageTypes.Add(new PackageType
+                {
+                    Name = NuGetPackageType.Dependency.Name,
+                    Version = NuGetPackageType.Dependency.Version.ToString(),
+                });
+            }
+
+            return packageTypes;
+        }
+
+        private static List<TargetFramework> GetTargetFrameworks(PackageArchiveReader packageReader)
+        {
+            var targetFrameworks = packageReader
+                .GetSupportedFrameworks()
+                .Select(f => new TargetFramework
+                {
+                    Moniker = f.GetShortFolderName()
+                })
+                .ToList();
+
+            // Default to the "any" framework if no frameworks were found.
+            if (targetFrameworks.Count == 0)
+            {
+                targetFrameworks.Add(new TargetFramework { Moniker = "any" });
+            }
+
+            return targetFrameworks;
         }
     }
 }
--- src/BaGet.Core/Indexing/PackageIndexingService.cs
+++ src/BaGet.Core/Indexing/PackageIndexingService.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
@@ -10,8 +8,6 @@ using NuGet.Packaging;
 
 namespace BaGet.Core
 {
-    using NuGetPackageType = NuGet.Packaging.Core.PackageType;
-
     public class PackageIndexingService : IPackageIndexingService
     {
         private readonly IPackageService _packages;
@@ -45,7 +41,7 @@ namespace BaGet.Core
             {
                 using (var packageReader = new PackageArchiveReader(packageStream, leaveStreamOpen: true))
                 {
-                    package = GetPackageMetadata(packageReader);
+                    package = packageReader.GetPackageMetadata();
                     nuspecStream = await packageReader.GetNuspecAsync(cancellationToken);
                     nuspecStream = await nuspecStream.AsTemporaryFileStreamAsync();
 
@@ -147,182 +143,6 @@ namespace BaGet.Core
                 package.NormalizedVersionString);
 
             return PackageIndexingResult.Success;
-        }
-
-        private Package GetPackageMetadata(PackageArchiveReader packageReader)
-        {
-            var nuspec = packageReader.NuspecReader;
-
-            (var repositoryUri, var repositoryType) = GetRepositoryMetadata(nuspec);
-
-            return new Package
-            {
-                Id = nuspec.GetId(),
-                Version = nuspec.GetVersion(),
-                Authors = ParseAuthors(nuspec.GetAuthors()),
-                Description = nuspec.GetDescription(),
-                HasReadme = packageReader.HasReadme(),
-                IsPrerelease = nuspec.GetVersion().IsPrerelease,
-                Language = nuspec.GetLanguage() ?? string.Empty,
-                Listed = true,
-                MinClientVersion = nuspec.GetMinClientVersion()?.ToNormalizedString() ?? string.Empty,
-                Published = DateTime.UtcNow,
-                RequireLicenseAcceptance = nuspec.GetRequireLicenseAcceptance(),
-                SemVerLevel = GetSemVerLevel(nuspec),
-                Summary = nuspec.GetSummary(),
-                Title = nuspec.GetTitle(),
-                IconUrl = ParseUri(nuspec.GetIconUrl()),
-                LicenseUrl = ParseUri(nuspec.GetLicenseUrl()),
-                ProjectUrl = ParseUri(nuspec.GetProjectUrl()),
-                RepositoryUrl = repositoryUri,
-                RepositoryType = repositoryType,
-                Dependencies = GetDependencies(nuspec),
-                Tags = ParseTags(nuspec.GetTags()),
-                PackageTypes = GetPackageTypes(nuspec),
-                TargetFrameworks = GetTargetFrameworks(packageReader),
-            };
-        }
-
-        // Based off https://github.com/NuGet/NuGetGallery/blob/master/src/NuGetGallery.Core/SemVerLevelKey.cs
-        private SemVerLevel GetSemVerLevel(NuspecReader nuspec)
-        {
-            if (nuspec.GetVersion().IsSemVer2)
-            {
-                return SemVerLevel.SemVer2;
-            }
-
-            foreach (var dependencyGroup in nuspec.GetDependencyGroups())
-            {
-                foreach (var dependency in dependencyGroup.Packages)
-                {
-                    if ((dependency.VersionRange.MinVersion != null && dependency.VersionRange.MinVersion.IsSemVer2)
-                        || (dependency.VersionRange.MaxVersion != null && dependency.VersionRange.MaxVersion.IsSemVer2))
-                    {
-                        return SemVerLevel.SemVer2;
-                    }
-                }
-            }
-
-            return SemVerLevel.Unknown;
-        }
-
-        private Uri ParseUri(string uriString)
-        {
-            if (string.IsNullOrEmpty(uriString)) return null;
-
-            return new Uri(uriString);
-        }
-
-        private string[] ParseAuthors(string authors)
-        {
-            if (string.IsNullOrEmpty(authors)) return new string[0];
-
-            return authors.Split(new[] { ',', ';', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-        }
-
-        private string[] ParseTags(string tags)
-        {
-            if (string.IsNullOrEmpty(tags)) return new string[0];
-
-            return tags.Split(new[] { ',', ';', ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-        }
-
-        private (Uri repositoryUrl, string repositoryType) GetRepositoryMetadata(NuspecReader nuspec)
-        {
-            var repository = nuspec.GetRepositoryMetadata();
-
-            if (string.IsNullOrEmpty(repository?.Url) ||
-                !Uri.TryCreate(repository.Url, UriKind.Absolute, out var repositoryUri))
-            {
-                return (null, null);
-            }
-
-            if (repositoryUri.Scheme != Uri.UriSchemeHttps)
-            {
-                return (null, null);
-            }
-
-            if (repository.Type.Length > 100)
-            {
-                throw new InvalidOperationException("Repository type must be less than or equal 100 characters");
-            }
-
-            return (repositoryUri, repository.Type);
-        }
-
-        private List<PackageDependency> GetDependencies(NuspecReader nuspec)
-        {
-            var dependencies = new List<PackageDependency>();
-
-            foreach (var group in nuspec.GetDependencyGroups())
-            {
-                var targetFramework = group.TargetFramework.GetShortFolderName();
-
-                if (!group.Packages.Any())
-                {
-                    dependencies.Add(new PackageDependency
-                    {
-                        Id = null,
-                        VersionRange = null,
-                        TargetFramework = targetFramework,
-                    });
-                }
-
-                foreach (var dependency in group.Packages)
-                {
-                    dependencies.Add(new PackageDependency
-                    {
-                        Id = dependency.Id,
-                        VersionRange = dependency.VersionRange?.ToString(),
-                        TargetFramework = targetFramework,
-                    });
-                }
-            }
-
-            return dependencies;
-        }
-
-        private List<PackageType> GetPackageTypes(NuspecReader nuspec)
-        {
-            var packageTypes = nuspec
-                .GetPackageTypes()
-                .Select(t => new PackageType
-                {
-                    Name = t.Name,
-                    Version = t.Version.ToString()
-                })
-                .ToList();
-
-            // Default to the standard "dependency" package type if no types were found.
-            if (packageTypes.Count == 0)
-            {
-                packageTypes.Add(new PackageType
-                {
-                    Name = NuGetPackageType.Dependency.Name,
-                    Version = NuGetPackageType.Dependency.Version.ToString(),
-                });
-            }
-
-            return packageTypes;
-        }
-
-        private List<TargetFramework> GetTargetFrameworks(PackageArchiveReader packageReader)
-        {
-            var targetFrameworks = packageReader
-                .GetSupportedFrameworks()
-                .Select(f => new TargetFramework
-                {
-                    Moniker = f.GetShortFolderName()
-                })
-                .ToList();
-
-            // Default to the "any" framework if no frameworks were found.
-            if (targetFrameworks.Count == 0)
-            {
-                targetFrameworks.Add(new TargetFramework { Moniker = "any" });
-            }
-
-            return targetFrameworks;
         }
     }
 }
