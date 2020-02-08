20508e316c90c645e2f207a59e39007c680ac731
Loic Sharma <sharma.loic@gmail.com>
2020-02-07 20:25:08 -0800

    SDK support BaGet

--- src/BaGet.Protocol/Extensions/ServiceIndexModelExtensions.cs
+++ src/BaGet.Protocol/Extensions/ServiceIndexModelExtensions.cs
@@ -8,7 +8,7 @@ namespace BaGet.Protocol
     /// </summary>
     public static class ServiceIndexModelExtensions
     {
-        // See: https://github.com/NuGet/NuGet.Client/blob/dev/src/NuGet.Core/NuGet.Protocol/Constants.cs
+        // See: https://github.com/NuGet/NuGet.Client/blob/e08358296db5bfa6f7f32d6f4ec8de288f3b0388/src/NuGet.Core/NuGet.Protocol/ServiceTypes.cs
         private static readonly string Version200 = "/2.0.0";
         private static readonly string Version300beta = "/3.0.0-beta";
         private static readonly string Version300 = "/3.0.0";
@@ -16,16 +16,19 @@ namespace BaGet.Protocol
         private static readonly string Version360 = "/3.6.0";
         private static readonly string Version470 = "/4.7.0";
         private static readonly string Version490 = "/4.9.0";
+        private static readonly string Version500 = "/5.0.0";
+        private static readonly string Version510 = "/5.1.0";
 
         private static readonly string[] Catalog = { "Catalog" + Version300 };
         private static readonly string[] SearchQueryService = { "SearchQueryService" + Version340, "SearchQueryService" + Version300beta, "SearchQueryService" };
         private static readonly string[] RegistrationsBaseUrl = { "RegistrationsBaseUrl" + Version360, "RegistrationsBaseUrl" + Version340, "RegistrationsBaseUrl" + Version300beta, "RegistrationsBaseUrl" };
         private static readonly string[] SearchAutocompleteService = { "SearchAutocompleteService", "SearchAutocompleteService" + Version300beta };
         private static readonly string[] ReportAbuse = { "ReportAbuseUriTemplate", "ReportAbuseUriTemplate" + Version300 };
+        private static readonly string[] PackageDetailsUriTemplate = { "PackageDetailsUriTemplate" + Version510 };
         private static readonly string[] LegacyGallery = { "LegacyGallery" + Version200 };
         private static readonly string[] PackagePublish = { "PackagePublish" + Version200 };
         private static readonly string[] PackageBaseAddress = { "PackageBaseAddress" + Version300 };
-        private static readonly string[] RepositorySignatures = { "RepositorySignatures" + Version490, "RepositorySignatures" + Version470 };
+        private static readonly string[] RepositorySignatures = { "RepositorySignatures" + Version500, "RepositorySignatures" + Version490, "RepositorySignatures" + Version470 };
         private static readonly string[] SymbolPackagePublish = { "SymbolPackagePublish" + Version490 };
 
         public static string GetPackageContentResourceUrl(this ServiceIndexResponse serviceIndex)
