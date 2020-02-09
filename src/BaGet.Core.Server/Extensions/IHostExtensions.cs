fbd5b06b335ad256071b810324f27a6e5b2b1407
Loïc Sharma <sharma.loic@gmail.com>
2020-02-09 15:35:27 -0800

    [PostgreSQL] No longer require restart after creating the database (#467)
    Npgsql, the PostgreSQL driver, caches the database's type information on the initial connection. This causes issues when BaGet creates the database as it adds the citext extension to support case insensitive columns. BaGet will now forcefully reload the database's types after migrations are run.
    
    See: https://github.com/loic-sharma/BaGet/issues/442
    See: https://github.com/npgsql/efcore.pg/issues/170#issuecomment-303417225

--- src/BaGet.Core.Server/Extensions/IHostExtensions.cs
+++ src/BaGet.Core.Server/Extensions/IHostExtensions.cs
@@ -21,10 +21,7 @@ namespace BaGet.Extensions
                 {
                     var ctx = scope.ServiceProvider.GetRequiredService<IContext>();
 
-                    // TODO: An "InvalidOperationException" is thrown and caught due to a bug
-                    // in EF Core 3.0. This is fixed in 3.1.
-                    // See: https://github.com/dotnet/efcore/issues/18307
-                    await ctx.Database.MigrateAsync(cancellationToken);
+                    await ctx.RunMigrationsAsync(cancellationToken);
                 }
             }
         }
