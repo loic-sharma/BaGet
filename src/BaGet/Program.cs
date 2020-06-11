a513445d47cbc97e1edeaa3469e87dc97748f6a3
SeppPenner <HansM2013@gmx.de>
2020-06-11 13:11:59 +0200

    Fixed duplicate namespace in BaGet program.

--- src/BaGet/Program.cs
+++ src/BaGet/Program.cs
@@ -10,73 +10,70 @@ using Microsoft.Extensions.Hosting;
 
 namespace BaGet
 {
-    namespace BaGet
+    public class Program
     {
-        public class Program
+        public static async Task Main(string[] args)
         {
-            public static async Task Main(string[] args)
+            var app = new CommandLineApplication
             {
-                var app = new CommandLineApplication
-                {
-                    Name = "baget",
-                    Description = "A light-weight NuGet service",
-                };
+                Name = "baget",
+                Description = "A light-weight NuGet service",
+            };
 
-                app.HelpOption(true);
+            app.HelpOption(true);
 
-                app.Command("import", import =>
+            app.Command("import", import =>
+            {
+                import.Command("downloads", downloads =>
                 {
-                    import.Command("downloads", downloads =>
+                    downloads.OnExecuteAsync(async cancellationToken =>
                     {
-                        downloads.OnExecuteAsync(async cancellationToken =>
-                        {
-                            var host = CreateHostBuilder(args).Build();
-                            var importer = host.Services.GetRequiredService<DownloadsImporter>();
-                            await importer.ImportAsync(cancellationToken);
-                        });
+                        var host = CreateHostBuilder(args).Build();
+                        var importer = host.Services.GetRequiredService<DownloadsImporter>();
+                        await importer.ImportAsync(cancellationToken);
                     });
                 });
+            });
 
-                app.OnExecuteAsync(async cancellationToken =>
-                {
-                    var host = CreateWebHostBuilder(args).Build();
-                    await host.RunMigrationsAsync(cancellationToken);
-                    await host.RunAsync(cancellationToken);
-                });
+            app.OnExecuteAsync(async cancellationToken =>
+            {
+                var host = CreateWebHostBuilder(args).Build();
+                await host.RunMigrationsAsync(cancellationToken);
+                await host.RunAsync(cancellationToken);
+            });
 
-                await app.ExecuteAsync(args);
-            }
-
-            public static IHostBuilder CreateWebHostBuilder(string[] args) =>
-                CreateHostBuilder(args)
-                    .ConfigureWebHostDefaults(web =>
-                    {
-                        web.ConfigureKestrel(options =>
-                        {
-                            // Remove the upload limit from Kestrel. If needed, an upload limit can
-                            // be enforced by a reverse proxy server, like IIS.
-                            options.Limits.MaxRequestBodySize = null;
-                        });
-
-                        var config = new ConfigurationBuilder()
-                            .SetBasePath(Directory.GetCurrentDirectory())
-                            .AddJsonFile("appsettings.json", true)
-                            .AddCommandLine(args)
-                            .Build();
-
-                        var urls = config["Urls"];
-
-                        if (!string.IsNullOrWhiteSpace(urls))
-                        {
-                            web.UseUrls(urls);
-                        }
-
-                        web.UseStartup<Startup>();
-                    });
-
-            public static IHostBuilder CreateHostBuilder(string[] args) =>
-                Host.CreateDefaultBuilder(args)
-                    .UseBaGet();
+            await app.ExecuteAsync(args);
         }
+
+        public static IHostBuilder CreateWebHostBuilder(string[] args) =>
+            CreateHostBuilder(args)
+                .ConfigureWebHostDefaults(web =>
+                {
+                    web.ConfigureKestrel(options =>
+                    {
+                        // Remove the upload limit from Kestrel. If needed, an upload limit can
+                        // be enforced by a reverse proxy server, like IIS.
+                        options.Limits.MaxRequestBodySize = null;
+                    });
+
+                    var config = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", true)
+                        .AddCommandLine(args)
+                        .Build();
+
+                    var urls = config["Urls"];
+
+                    if (!string.IsNullOrWhiteSpace(urls))
+                    {
+                        web.UseUrls(urls);
+                    }
+
+                    web.UseStartup<Startup>();
+                });
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseBaGet();
     }
 }
