0cb78fb4f72ed81f639c63eec08258feb92aef26
maurício sipmann <sipmann@gmail.com>
2020-06-14 20:43:14 -0400

    Tests working again

--- src/BaGet.UI/src/App.test.tsx
+++ src/BaGet.UI/src/App.test.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
+import { StaticRouter as Router } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Router><App /></Router>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
