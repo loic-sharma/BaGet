47b48d3187c55b08555ea4b60fc6f70afd48f48f
Loic Sharma <sharma.loic@gmail.com>
2020-07-19 13:25:24 -0700

    Make time deterministic; remove debugger breaks due to caught exceptions

--- tests/BaGet.Tests/Support/BaGetWebApplicationFactory.cs
+++ tests/BaGet.Tests/Support/BaGetWebApplicationFactory.cs
@@ -6,6 +6,8 @@ using BaGet.Core;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -16,12 +18,28 @@ namespace BaGet.Tests
 {
     public class BaGetWebApplicationFactory : WebApplicationFactory<Startup>
     {
+        private readonly Mock<SystemTime> _time;
+
+        public BaGetWebApplicationFactory()
+        {
+            _time = new Mock<SystemTime>();
+            _time
+                .Setup(t => t.UtcNow)
+                .Returns(DateTime.Parse("2020-01-01T00:00:00.000Z"));
+        }
+
         public WebApplicationFactory<Startup> WithOutput(ITestOutputHelper output)
         {
             return WithWebHostBuilder(builder =>
             {
                 builder.ConfigureLogging(logging =>
                 {
+                    // BaGet uses console logging by default. This logger throws operation
+                    // cancelled exceptions when the host shuts down, causing the the debugger
+                    // to pause repeatedly if CLR exceptions are enabled.
+                    logging.ClearProviders();
+
+                    // Pipe logs to the xunit output.
                     logging.AddProvider(new XunitLoggerProvider(output));
                 });
             });
@@ -41,6 +59,7 @@ namespace BaGet.Tests
 
             builder
                 .UseStartup<Startup>()
+                .UseEnvironment("Production")
                 .ConfigureAppConfiguration(config =>
                 {
                     // Setup the integration test configuration.
@@ -56,13 +75,8 @@ namespace BaGet.Tests
                 })
                 .ConfigureServices((context, services) =>
                 {
-                    // Clobber BaGet services with test replacements.
-                    var time = new Mock<SystemTime>();
-                    time
-                        .Setup(t => t.UtcNow)
-                        .Returns(DateTime.Parse("2020-01-01T00:00:00.000Z"));
-
-                    services.AddSingleton(time.Object);
+                    // Add mocks for testing purposes.
+                    services.AddSingleton(_time.Object);
 
                     // Setup the integration test database.
                     var provider = services.BuildServiceProvider();
@@ -70,14 +84,22 @@ namespace BaGet.Tests
 
                     using (var scope = scopeFactory.CreateScope())
                     {
+                        // Ensure the database is created before we run migrations. The migrations
+                        // can create the database too, however, migrations check whether the database exists
+                        // first. The SQLite provider implements this by attempting to open a connection,
+                        // and if that fails, creating the database. This throws several exceptions that
+                        // pauses the debugger repeatedly if CLR exceptions are enabled.
+                        // See: https://github.com/dotnet/efcore/blob/644d3c8c3a604fd0121d90eaf34f14870e19bcff/src/EFCore.Sqlite.Core/Storage/Internal/SqliteDatabaseCreator.cs#L88-L98
                         var ctx = scope.ServiceProvider.GetRequiredService<IContext>();
-                        var indexer = scope.ServiceProvider.GetRequiredService<IPackageIndexingService>();
-                        var cancellationToken = CancellationToken.None;
+                        var dbCreator = ctx.Database.GetService<IRelationalDatabaseCreator>();
 
-                        // Ensure the database is created.
+                        dbCreator.Create();
                         ctx.Database.Migrate();
 
                         // Seed the application with test data.
+                        var indexer = scope.ServiceProvider.GetRequiredService<IPackageIndexingService>();
+                        var cancellationToken = CancellationToken.None;
+
                         var result = indexer.IndexAsync(PackageData.Default, cancellationToken).Result;
                         if (result != PackageIndexingResult.Success)
                         {
