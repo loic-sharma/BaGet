bd4f2dedce6dd60718414baf8b7faad78619246c
David Ackroyd <davidackroydmusic@gmail.com>
2020-08-23 14:25:30 +0000

    icons

--- src/BaGet.UI/src/DisplayPackage/RepoDetails.tsx
+++ src/BaGet.UI/src/DisplayPackage/RepoDetails.tsx
@@ -1,3 +1,4 @@
+import { Icon } from 'office-ui-fabric-react/lib/Icon'
 import * as React from 'react';
 
 interface IRepoDetailsProps {
@@ -39,9 +40,12 @@ class RepoDetails extends React.Component<IRepoDetailsProps, IRepoDetailsState> 
       <div>
         <h2>Project Details</h2>
         
-        <p>Open issues:{this.state.openIssues}</p>
-        <p>Open pulls:{this.state.openPulls}</p>
-        <p>Last commit:{this.state.lastCommit.toLocaleDateString()}</p>
+        <div className="project-stats">
+          <span><Icon iconName="Error" className="ms-Icon" />{this.state.openIssues}</span>
+          <span><Icon iconName="BranchPullRequest" className="ms-Icon" />{this.state.openPulls}</span>
+        </div>
+        
+        <p className="last-committed">Last committed on {this.state.lastCommit.toLocaleDateString()}</p>
       </div>
     );
   }
