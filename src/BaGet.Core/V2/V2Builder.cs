cfe531547643dfd0dc131cfd83c85d3144dfbaad
Loic Sharma <sharma.loic@gmail.com>
2021-12-12 16:39:44 -0800

    Improve output

--- src/BaGet.Core/V2/V2Builder.cs
+++ src/BaGet.Core/V2/V2Builder.cs
@@ -79,14 +79,15 @@ namespace BaGet.Core
                 new XAttribute(N.d, NS.d),
                 new XAttribute(N.georss, NS.georss),
                 new XAttribute(N.gml, NS.gml),
-                new XElement(N.id, packageV2Url),
-                new XElement(N.title, package.Title),
 
+                new XElement(N.id, packageV2Url),
                 new XElement(
                     N.content,
                     new XAttribute("type", "application/zip"),
                     new XAttribute("src", downloadUrl)
                 ),
+                new XElement(N.summary, package.Summary),
+                new XElement(N.title, package.Title),
 
                 BuildAuthor(package),
                 BuildProperties(package)
@@ -98,10 +99,6 @@ namespace BaGet.Core
             // See: https://joelverhagen.github.io/NuGetUndocs/#package-entity
             return new XElement(
                 N.m_properties,
-                new XElement(N.d_Id, package.Id),
-                new XElement(N.d_Title, package.Title),
-                new XElement(N.d_Version, package.OriginalVersionString),
-                new XElement(N.d_NormalizedVersion, package.NormalizedVersionString),
                 new XElement(N.d_Authors, string.Join(", ", package.Authors)),
                 new XElement(N.d_Copyright, ""), // TODO
                 new XElement(N.d_Description, package.Description),
@@ -109,16 +106,24 @@ namespace BaGet.Core
                     N.d_DownloadCount,
                     new XAttribute(N.m_type, "Edm.Int32"),
                     package.Downloads),
+                new XElement(N.d_IconUrl, package.IconUrl), // TODO, URL logic
+                new XElement(N.d_Id, package.Id),
+                new XElement(N.d_Language, package.Language),
                 new XElement(N.d_LastEdited, package.Published),
-                new XElement(N.d_Published, package.Published),
+                new XElement(N.d_LicenseUrl, package.LicenseUrl), // TODO
+                new XElement(N.d_MinClientVersion, package.MinClientVersion),
+                new XElement(N.d_NormalizedVersion, package.NormalizedVersionString),
                 new XElement(N.d_PackageHash, ""),
                 new XElement(N.d_PackageHashAlgorithm, ""),
                 new XElement(N.d_PackageSize, 0),
                 new XElement(N.d_ProjectUrl, package.ProjectUrl),
-                new XElement(N.d_IconUrl, package.IconUrl), // TODO, URL logic
-                new XElement(N.d_LicenseUrl, package.LicenseUrl), // TODO
-                new XElement(N.d_Tags, string.Join(", ", package.Tags)),
+                new XElement(N.d_Published, package.Published),
+                new XElement(N.d_ReleaseNotes, package.ReleaseNotes),
                 new XElement(N.d_RequireLicenseAcceptance, package.RequireLicenseAcceptance),
+                new XElement(N.d_Summary, package.Summary),
+                new XElement(N.d_Tags, string.Join(" ", package.Tags)),
+                new XElement(N.d_Title, package.Title),
+                new XElement(N.d_Version, package.OriginalVersionString),
 
                 BuildDependencies(package)
             );
@@ -126,10 +131,9 @@ namespace BaGet.Core
 
         private XElement BuildAuthor(Package package)
         {
-            // TODO: No authors?
             return new XElement(
                 N.author,
-                package.Authors.Select(author => new XElement(N.name, author))
+                new XElement(N.name, string.Join(", ", package.Authors))
             );
         }
 
@@ -164,6 +168,7 @@ namespace BaGet.Core
         private static class NS
         {
             public static readonly XNamespace xmlns = "http://www.w3.org/2005/Atom";
+            // TODO: Remove?
             //public static readonly XNamespace baze = "https://www.nuget.org/api/v2/curated-feeds/microsoftdotnet";
             public static readonly XNamespace m = "http://schemas.microsoft.com/ado/2007/08/dataservices/metadata";
             public static readonly XNamespace d = "http://schemas.microsoft.com/ado/2007/08/dataservices";
@@ -181,33 +186,37 @@ namespace BaGet.Core
             public static readonly XName link = NS.xmlns + "link";
             public static readonly XName id = NS.xmlns + "id";
             public static readonly XName content = NS.xmlns + "content";
+            public static readonly XName summary = NS.xmlns + "summary";
 
             public static readonly XName m_count = NS.m + "count";
             public static readonly XName m_properties = NS.m + "properties";
             public static readonly XName m_type = NS.m + "type";
 
-            public static readonly XName d_Id = NS.d + "Id";
-            public static readonly XName d_Title = NS.d + "Title";
-            public static readonly XName d_Version = NS.d + "Version";
-            public static readonly XName d_NormalizedVersion = NS.d + "NormalizedVersion";
             public static readonly XName d_Authors = NS.d + "Authors";
             public static readonly XName d_Copyright = NS.d + "Copyright";
+            public static readonly XName d_Created = NS.d + "Created";
             public static readonly XName d_Dependencies = NS.d + "Dependencies";
             public static readonly XName d_Description = NS.d + "Description";
-            public static readonly XName d_IconUrl = NS.d + "IconUrl";
-            public static readonly XName d_LicenseUrl = NS.d + "LicenseUrl";
-            public static readonly XName d_ProjectUrl = NS.d + "ProjectUrl";
-            public static readonly XName d_Tags = NS.d + "Tags";
-            public static readonly XName d_ReportAbuseUrl = NS.d + "ReportAbuseUrl";
-            public static readonly XName d_RequireLicenseAcceptance = NS.d + "RequireLicenseAcceptance";
             public static readonly XName d_DownloadCount = NS.d + "DownloadCount";
-            public static readonly XName d_Created = NS.d + "Created";
+            public static readonly XName d_IconUrl = NS.d + "IconUrl";
+            public static readonly XName d_Id = NS.d + "Id";
+            public static readonly XName d_Language = NS.d + "Language";
             public static readonly XName d_LastEdited = NS.d + "LastEdited";
-            public static readonly XName d_Published = NS.d + "Published";
+            public static readonly XName d_LicenseUrl = NS.d + "LicenseUrl";
+            public static readonly XName d_MinClientVersion = NS.d + "MinClientVersion";
+            public static readonly XName d_NormalizedVersion = NS.d + "NormalizedVersion";
             public static readonly XName d_PackageHash = NS.d + "PackageHash";
             public static readonly XName d_PackageHashAlgorithm = NS.d + "PackageHashAlgorithm";
-            public static readonly XName d_MinClientVersion = NS.d + "MinClientVersion";
             public static readonly XName d_PackageSize = NS.d + "PackageSize";
+            public static readonly XName d_ProjectUrl = NS.d + "ProjectUrl";
+            public static readonly XName d_Published = NS.d + "Published";
+            public static readonly XName d_ReleaseNotes = NS.d + "ReleaseNotes";
+            public static readonly XName d_ReportAbuseUrl = NS.d + "ReportAbuseUrl";
+            public static readonly XName d_RequireLicenseAcceptance = NS.d + "RequireLicenseAcceptance";
+            public static readonly XName d_Summary = NS.d + "Summary";
+            public static readonly XName d_Tags = NS.d + "Tags";
+            public static readonly XName d_Title = NS.d + "Title";
+            public static readonly XName d_Version = NS.d + "Version";
 
             public static readonly XName baze = XNamespace.Xmlns + "base";
             public static readonly XName m = XNamespace.Xmlns + "m";
