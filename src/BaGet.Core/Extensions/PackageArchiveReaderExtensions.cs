13c3cd34b924b30b286f51ea3b5d9eb3c84dd0f5
Dominic Schröder <dominic.schroeder@quest.com>
2023-07-05 02:08:07 +0200

    using StripLeadingDirectorySeparators on readmePath as already done on iconPath

--- src/BaGet.Core/Extensions/PackageArchiveReaderExtensions.cs
+++ src/BaGet.Core/Extensions/PackageArchiveReaderExtensions.cs
@@ -29,7 +29,9 @@ namespace BaGet.Core
                 throw new InvalidOperationException("Package does not have a readme!");
             }
 
-            return await package.GetStreamAsync(readmePath, cancellationToken);
+            return await package.GetStreamAsync(
+                PathUtility.StripLeadingDirectorySeparators(readmePath), 
+                cancellationToken);
         }
 
         public async static Task<Stream> GetIconAsync(
