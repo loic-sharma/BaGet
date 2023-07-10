f56a58e7753730ef093729d55e0dd7d52395a3df
Shamus Taylor <Shamus03@me.com>
2023-07-10 14:58:55 +0000

    Add migration

---
+++ src/BaGet.Database.MySql/Migrations/20230710145506_CreatePackageContentsTable.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BaGet.Database.MySql.Migrations
+{
+    public partial class CreatePackageContentsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RowVersion",
+                table: "Packages",
+                rowVersion: true,
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp(6)",
+                oldNullable: true)
+                .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+
+            migrationBuilder.CreateTable(
+                name: "PackageContents",
+                columns: table => new
+                {
+                    Key = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Path = table.Column<string>(maxLength: 404, nullable: true),
+                    Data = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PackageContents", x => x.Key);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PackageContents_Path",
+                table: "PackageContents",
+                column: "Path",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PackageContents");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "RowVersion",
+                table: "Packages",
+                type: "timestamp(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldRowVersion: true,
+                oldNullable: true)
+                .OldAnnotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn);
+        }
+    }
+}
