44b96f392b46f6b1707f8490c6871e64212a942a
Kannan Chithambaranathan <ckannan759153@gmail.com>
2023-10-13 22:06:28 +0530

    Unified .netcore3.1, .net7.0, .net8.0

--- src/BaGet.Database.MySql/GlobalUsings.cs
+++ src/BaGet.Database.MySql/GlobalUsings.cs
@@ -7,4 +7,7 @@ global using Microsoft.EntityFrameworkCore.Metadata;
 global using Microsoft.EntityFrameworkCore.Migrations;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.Extensions.Options;
+
+#if !NETCOREAPP3_1
 global using MySqlConnector;
+#endif
